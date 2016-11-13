fa5c7d4a3162b5a6f15c0f25396338418fdd44cc
stepbyst3p <spikerdn@gmail.com>
2016-11-13 17:26:01 +0500

    created form component

--- scripts/components/AddPlayerForm.js
+++ scripts/components/AddPlayerForm.js
@@ -3,16 +3,31 @@ import React from 'react'
 
 export default class AddingPlayerForm extends React.Component {
     
-    handleSubmit(e) {
-    	e.preventDefault();		
+    
+
+	constructor() {
+		super();
+
+	 	this.handleInputChange = this.handleInputChange.bind(this);
+	 	this.handleSubmit = this.handleSubmit.bind(this);			   
+	   	this.state = {name: ''};
+	 }
+
+	handleSubmit(e) {			
+    	e.preventDefault();
+    	alert('We are gonna add Player with name ' + this.state.name)
 	}
 
-    render() {    	
+	handleInputChange(e) {		
+		this.setState({ name: e.target.value }) 		
+	}
+
+    render() {    	    	
         return (
         	<div>
         		<h2>Adding Players</h2>
         		<form onSubmit={this.handleSubmit}>
-        			<input type="text" />
+        			<input onChange={this.handleInputChange} type="text" />
         			<button type="submit">Add Player</button>
         		</form>
         	</div>

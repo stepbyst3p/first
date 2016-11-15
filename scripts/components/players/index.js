697d3395d5946d4608e5f2607b7f26b7135d2c4c
Edele Gizatullin <me@edele.ru>
2016-11-15 21:43:49 +0500

    Renamed component

--- scripts/components/players/index.js
+++ scripts/components/players/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PlayersForm from './form'
 
-export default class AddingPlayerForm extends React.Component {
+export default class Players extends React.Component {
         
 	constructor() {
 		super();	 
